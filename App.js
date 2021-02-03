b37769a75111cd12f42bfc75b47a07d45f2d3361
humblesami <samiakram@live.com>
2021-02-03 16:23:35 +0500

    added screens

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BoardScreen from './components/BoardScreen';
+import BoardDetailScreen from './components/BoardDetailScreen';
+import AddBoardScreen from './components/AddBoardScreen';
+import EditBoardScreen from './components/EditBoardScreen';
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Home!</Text>
+            </View>
+        );
+    }
+}
+
+const BoardNavigator = createStackNavigator({
+    Board: BoardScreen,
+    BoardDetails: BoardDetailScreen,
+    AddBoard: AddBoardScreen,
+    EditBoard: EditBoardScreen,
+});
+
+const AppNavigator = createSwitchNavigator({
+    Board: BoardNavigator,
+    Home: HomeScreen,
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+export default class App extends React.Component {
+    render() {
+        return <AppContainer />;
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
